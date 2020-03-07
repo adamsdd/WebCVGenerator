85b7f77f805f3b61d41277e85e6d9fd041adc007
Dawid Adamczyk <adadawdd@gmail.com>
2020-03-07 15:51:30 +0100

    Create project with modules

---
+++ WebCVGenerator-server/src/main/java/adamsdd/webcvgenerator/WebCvGeneratorApplication.java
@@ -0,0 +1,13 @@
+package adamsdd.webcvgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCvGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCvGeneratorApplication.class, args);
+    }
+
+}
---
+++ WebCVGenerator-server/src/test/java/adamsdd/webcvgenerator/WebCvGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package adamsdd.webcvgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCvGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
